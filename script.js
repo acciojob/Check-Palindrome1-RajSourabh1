0dbac23ad3af70a17d9810a492c0efb42941fca7
Sourabh Kumar <createownpath111@gmail.com>
2023-03-17 19:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+for(let i=0;i<str.length;i++){
+	for(let j=str.length-1;j>=0;j--){
+		if(str.charAt(i)!=str.charAt(j))
+			return false;
+	}
+  }
+	return true;
 }
 module.exports = palindrome
